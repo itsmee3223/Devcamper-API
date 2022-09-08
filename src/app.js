aa65c0a71358a71db0a38cfbfaff69cd6d42f84b
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-08 16:47:46 +0700

    setup server

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
-const path = require('path');
+const path = require("path");
 
+const morgan = require("morgan");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
 const express = require("express");
-require('dotenv').config({path: path.join(__dirname, './config/.env')})
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: path.join(__dirname, "./config/.env") });
 
-const app =  express();
+const app = express();
+const limiter = rateLimit({
+  windowMS: 10 * 60 * 1000,
+  max: 100,
+});
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("combined"));
+}
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(limiter);
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-module.exports = app
+module.exports = app;
