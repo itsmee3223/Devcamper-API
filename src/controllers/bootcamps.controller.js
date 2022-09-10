391c615ac7604374b96a8501bf577d68b463d6e3
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-10 16:31:36 +0700

    /GET bootcamps by zipcode within some radius

--- src/controllers/bootcamps.controller.js
+++ src/controllers/bootcamps.controller.js
@@ -2,6 +2,7 @@ const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
 const BootcampSchema = require("../models/Bootcamp.schema");
+const geocoder = require("../utils/geocoder");
 
 exports.httpGetBootcamps = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
@@ -17,4 +18,24 @@ exports.httpGetBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: bootcamp });
+});
+
+exports.httpGetBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  const location = await geocoder.geocode(zipcode);
+  const latitude = location[0].latitude;
+  const longitude = location[0].longitude;
+  const radius = distance / 6357;
+  const bootcamps = await BootcampSchema.find({
+    location: {
+      $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
--- src/routes/bootcamps.route.js
+++ src/routes/bootcamps.route.js
@@ -5,9 +5,11 @@ const BootcampSchema = require("../models/Bootcamp.schema");
 const {
   httpGetBootcamps,
   httpGetBootcamp,
+  httpGetBootcampsInRadius,
 } = require("../controllers/bootcamps.controller");
 const advancedResults = require("../middleware/advancedResults");
 
 router.get("/", advancedResults(BootcampSchema), httpGetBootcamps);
+router.get("/radius/:zipcode/:distance", httpGetBootcampsInRadius);
 router.get("/:id", httpGetBootcamp);
 module.exports = router;
