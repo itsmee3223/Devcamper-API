5bebb296da7b7afa2fe7a656ac5e19f64baff8ba
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-10 09:32:24 +0700

    add seeder

---
+++ src/seeder.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const conncetDB = require("./config/database/db.mongo");
+require("dotenv").config({ path: path.join(__dirname, "./config/.env") });
+
+const Bootcamp = require("./models/Bootcamp/Bootcamp.schema");
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+const importData = async () => {
+  try {
+    await conncetDB();
+    await Bootcamp.create(bootcamps);
+    console.info("Data imported....");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await conncetDB();
+    await Bootcamp.deleteMany();
+    console.info("Data deleted....");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === "-d") return deleteData();
+
+importData();
