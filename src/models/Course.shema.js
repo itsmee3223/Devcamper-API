6a19cfab686b07a5614f07036e6deefdcf11f262
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-10 19:47:33 +0700

    Course model

---
+++ src/models/Course.shema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const CourseSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add a course title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a course description"],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add a number of weeks"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "Please add a tuituion cost"],
+  },
+  minimuSkill: {
+    type: String,
+    required: [true, "Please add a minimum skill"],
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Bootcamp",
+    required: true,
+  },
+});
+
+CourseSchema.statics.getAverageCost = async (bootcampId) => {
+  const obj = this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+CourseSchema.post("save", () => {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+CourseSchema.pre("remove", () => {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
