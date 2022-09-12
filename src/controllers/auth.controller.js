5d0e1de403265ba4b5d95bb9725633816d74dff1
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-12 21:41:53 +0700

    /GET Logout User

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,3 +33,15 @@ exports.httpLoginUser = asyncHandler(async (req, res, next) => {
 
   sendTokenResponse(user, 200, res);
 });
+
+exports.httpLogoutUser = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 + 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    succsess: true,
+    data: {},
+  });
+});
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 const {
   httpRegisterUser,
   httpLoginUser,
+  httpLogoutUser,
 } = require("../controllers/auth.controller");
 
 const router = require("express").Router();
 
 router.post("/register", httpRegisterUser);
 router.post("/login", httpLoginUser);
+router.get("/logout", httpLogoutUser);
 
 module.exports = router;
